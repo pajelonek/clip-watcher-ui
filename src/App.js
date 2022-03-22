7c9a1d5de8de47137c67b6af56ed1d58dca0a021
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-03-22 22:37:48 +0100

    Initial commit for placeholder darkmode theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from "./Pages/Header";
+import Pricing from "./Pages/Pricing";
 
 function App() {
   return (
       <div className="App">
-        <Header/>
+        <Pricing/>
       </div>
   );
 }
