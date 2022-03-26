c39294efd165961db2f2f7c49984b9944189f212
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-03-26 17:07:51 +0100

    Deleting not healthy dependency

--- src/Components/MainPage/ClipsContainer.js
+++ src/Components/MainPage/ClipsContainer.js
@@ -1,15 +1,13 @@
 import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import * as React from "react";
 import {useEffect, useState} from "react";
 import Clip from "./Clip";
-import { usePromiseTracker, trackPromise } from "react-promise-tracker";
 import BouncingDotsLoader from "./BouncingDotsLoader";
 
 export default function ClipsContainer() {
     const [clips, setClips] = useState(null);
-    const { promiseInProgress } = usePromiseTracker();
+    const [loadingClips, setLoadingClips] = useState(false);
 
     const fetchClips = async () => {
         const requestOptions = {
@@ -24,15 +22,17 @@ export default function ClipsContainer() {
             })
         };
 
-        await trackPromise(
-            fetch(process.env.REACT_APP_API_URL + '/clips', requestOptions)
+        setLoadingClips(true);
+
+        await fetch(process.env.REACT_APP_API_URL + '/clips', requestOptions)
                 .then(r => r.json())
                 .then(r => setClips(r.data))
                 .catch(e => {
                     console.log('error');
                     console.log(e);
-                })
-            );
+                }).finally(() => {
+                setLoadingClips(false);
+                });
     };
 
     useEffect(() => {
@@ -42,10 +42,10 @@ export default function ClipsContainer() {
     return (
         <Container component="main">
             <Grid container spacing={5} alignItems="flex-end">
-                { clips && !promiseInProgress ?
+                {clips && !loadingClips ?
                     clips.map((clip) => (
                         <Grid item key={clip.id} xs={6} sm={4} md={3}>
-                            <Clip clip={clip} />
+                            <Clip clip={clip}/>
                         </Grid>
                     )) :
                     <Grid item key={'loader'} xs={12}>
@@ -53,7 +53,6 @@ export default function ClipsContainer() {
                     </Grid>
                 }
             </Grid>
-            <Button >Open modal</Button>
         </Container>
     );
 }
