33178ec6e1f557ea0f90e8ff3e9bfc36b0edfde1
pajelonek <pawel.jelonek.krakow@gmail.com>
2022-03-27 01:42:35 +0100

    Adding sidebar placeholder

--- src/Components/MainPage/ClipsContainer.js
+++ src/Components/MainPage/ClipsContainer.js
@@ -4,6 +4,7 @@ import * as React from "react";
 import {useEffect, useState} from "react";
 import Clip from "./Clip";
 import BouncingDotsLoader from "./BouncingDotsLoader";
+import {FormControl, InputLabel, NativeSelect} from "@mui/material";
 
 export default function ClipsContainer() {
     const [clips, setClips] = useState(null);
@@ -25,14 +26,14 @@ export default function ClipsContainer() {
         setLoadingClips(true);
 
         await fetch(process.env.REACT_APP_API_URL + '/clips', requestOptions)
-                .then(r => r.json())
-                .then(r => setClips(r.data))
-                .catch(e => {
-                    console.log('error');
-                    console.log(e);
-                }).finally(() => {
+            .then(r => r.json())
+            .then(r => setClips(r.data))
+            .catch(e => {
+                console.log('error');
+                console.log(e);
+            }).finally(() => {
                 setLoadingClips(false);
-                });
+            });
     };
 
     useEffect(() => {
@@ -40,19 +41,44 @@ export default function ClipsContainer() {
     }, []);
 
     return (
-        <Container component="main">
-            <Grid container spacing={5} alignItems="flex-end">
-                {clips && !loadingClips ?
-                    clips.map((clip) => (
-                        <Grid item key={clip.id} xs={6} sm={4} md={3}>
-                            <Clip clip={clip}/>
+        <Container component="main" maxWidth={"xl"}>
+            <Grid container spacing={5}>
+                <Grid item key={'menu'} xs={10}>
+                    <Grid container xs={12} spacing={5} alignItems="flex-end">
+                    {clips && !loadingClips ?
+                        clips.map((clip) => (
+                            <Grid item key={clip.id} xs={6} sm={4} md={3}>
+                                <Clip clip={clip}/>
+                            </Grid>
+                        )) :
+                        <Grid item key={'loader'} xs={12}>
+                            <BouncingDotsLoader/>
                         </Grid>
-                    )) :
-                    <Grid item key={'loader'} xs={12}>
-                        <BouncingDotsLoader/>
-                    </Grid>
-                }
+                    }
+                </Grid>
+                </Grid>
+                <Grid item key={'sideMenu'} xs={2}>
+                    <Grid container xs={12}>
+                    <FormControl fullWidth>
+                        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                            Age
+                        </InputLabel>
+                        <NativeSelect
+                            defaultValue={30}
+                            inputProps={{
+                                name: 'age',
+                                id: 'uncontrolled-native',
+                            }}
+                        >
+                            <option value={10}>Ten</option>
+                            <option value={20}>Twenty</option>
+                            <option value={30}>Thirty</option>
+                        </NativeSelect>
+                    </FormControl>
+                </Grid>
+                </Grid>
             </Grid>
+
         </Container>
     );
 }
